1780e9d0f4402026e08114c07c0fd20f452ca679
Mauricio Barbosa Pinto <mauricio.pinto@opus-software.com.br>
2022-11-21 15:48:51 -0300

    Aula 2 - Variaveis

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 var welcome = "Hello Javascript - Automação Full Stack from QA Ninja"
 document.getElementById("welcome").append(welcome)
 console.log("welcome")
+
+// Variavel é um local na memória do computador onde você pode alocar valores
+// que poderão ser alterados durante a execução do software
+// por isso chamamos de variaveis
+
+// tipagem dinâmica, ou seja, atribuí o tipo  quando recebe o valor
+var number = 10; 
+console.log(number)
+
+// typeof, que é responsavel por trazer o tipo da variavel
+
+console.log(typeof number);
+
+var name = "Maurício";
+console.log(typeof name);
+
+var n1 = 10;
+var n2 = 20;
+
+n1 = "1" + 5
+
+console.log(n1 + n2);
+
